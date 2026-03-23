3b94cbb8aae50586dfff7639ce445b023d1e1586
Ryo Lambert <hi@ryolambert.dev>
2026-03-23 17:57:44 -0600

    refactor: update build.bat references kirolink→openkiro

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 REM 编译
-go build -o kirolink.exe kirolink.go
+go build -o openkiro.exe openkiro.go
 IF %ERRORLEVEL% NEQ 0 (
     echo 编译失败!
     pause
     exit /b 1
 )
 REM 压缩
-upx --best --lzma kirolink.exe
+upx --best --lzma openkiro.exe
  
